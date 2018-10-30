51576515a0460d2b103e6db4af281dd272107d82
michael mwasela <dennysmycall@gmail.com>
2018-10-31 00:09:36 +0300

    ESkills4Girls2018 Project demo codes
    Fill free to clone the repo and run the programs on your own, :) Mwasela

---
+++ Automatic_Street_Light_Opertion/Automatic_Street_Light_Opertion.ino
@@ -0,0 +1,23 @@
+
+int sensorPin = A0;
+int sensorValue = 0;
+int ledPin = 8;
+
+void setup() {
+   Serial.begin(9600);
+   pinMode(sensorPin, INPUT); 
+  pinMode(ledPin, OUTPUT);
+}
+ 
+void loop() {
+   sensorValue = analogRead(sensorPin);
+   Serial.println(sensorValue);
+
+   if (sensorValue < 200){
+    digitalWrite(ledPin, LOW);
+       
+       }else if(sensorValue > 200){
+       digitalWrite(ledPin, HIGH); 
+      
+        }
+   }
